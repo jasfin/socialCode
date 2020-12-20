4840250ee2eb03e0381c0a66391b8765a4c626e4
jasfin <mazharjasfin@gmail.com>
2020-12-20 17:30:43 +0530

    chat amoung users implemented with socket.io

--- assets/js/chat_socket_FE.js
+++ assets/js/chat_socket_FE.js
@@ -1,9 +1,10 @@
 //this is the client side socket part where client establishes the connection
 class ChatEngine{
-    constructor(chatBoxId, userEmail){
+    constructor(chatBoxId, userEmail, userName){
         console.log('inside constructor');
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
+        this.userName = userName;
 
         //io.connect will emit an event connection which is detected in the backend socket part
         //this.socket = io.connect('http://localhost:5000');
@@ -20,14 +21,71 @@ class ChatEngine{
         if (this.userEmail){
             this.connectionHandler();
         }
-
     }
 
     
     connectionHandler(){
+        let self = this;
         //after the io.connect() call is detected on server side, server sends an acknowledgement which is caught here
         this.socket.on('connect', function(){
             console.log('connection established using sockets...!');
+            self.socket.emit('join_room', {
+                user_email: self.userEmail,
+                chatroom: 'socialCode',
+                user_name: self.userName
+            });
+
+            self.socket.on('user_joined', function(data){
+                console.log('a user joined!', data);
+                //below code is to inform all members of the chatroom that a new member joined
+                let newMessage = $('<li>');                
+                if (data.user_email != self.userEmail){
+                    let messageType = 'other-message';
+                    newMessage.append($('<span>', {
+                        'html': `${data.user_name} has joined the chat`
+                    }));
+                    newMessage.append($('<sub>', {
+                        'html': data.user_name
+                    }));
+                    newMessage.addClass(messageType);
+                    $('#chat-messages-list').append(newMessage);                   
+                }       
+
+            })
+        });
+
+        $('#send-message').click(function(){
+            let msg = $('#chat-message-input').val();
+            if (msg!=''){
+                self.socket.emit('send_message', {
+                    message: msg,
+                    user_email: self.userEmail,
+                    user_name: self.userName,
+                    chatroom: 'socialCode'
+                });
+            }
+        });
+
+        self.socket.on('receive_message', function(data){
+            console.log('message received is:', data.message);
+            let newMessage = $('<li>');
+            let messageType = 'other-message';
+            if (data.user_email == self.userEmail){
+                messageType = 'self-message';
+            }
+            newMessage.append($('<span>', {
+                'html': data.message
+            }));
+            newMessage.append($('<sub>', {
+                'html': data.user_name
+            }));
+            // id='message-holder'
+            newMessage.addClass(messageType);
+            let newDivMsg = $("<div>",{
+                id:messageType
+            });
+            newDivMsg.append(newMessage);
+            $('#chat-messages-list').append(newDivMsg);
         });
     }
 }
--- config/chat_socket_BE.js
+++ config/chat_socket_BE.js
@@ -2,11 +2,22 @@
 module.exports.chatSocket = function(socketServer){
     let io = require('socket.io')(socketServer);
     //when client sends a connect event , its detected here.., after detecting it sends back an acknowledgement that is
-       //detected inside - this.socket.on('connect' on the client side
+    //detected inside - this.socket.on('connect' on the client side
     io.sockets.on('connection', function(socket){
         console.log('new connection received', socket.id);
         socket.on('disconnect', function(){
             console.log('socket disconnected!');
+        });
+        //when FE emits a join_room event its detected here and we create a chat-room in the socket
+        socket.on('join_room', function(data){
+            console.log('received a joining request:', data);
+            socket.join(data.chatroom);
+            io.in(data.chatroom).emit('user_joined', data);
+        });
+        //when FE emits a send_message event, that message is broadcasted to all users in that chatroom
+        socket.on('send_message', function(data){
+            console.log('sending the message:',data);
+            io.in(data.chatroom).emit('receive_message', data);
         });
     });
 }
